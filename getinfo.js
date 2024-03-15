6e84376e303d2543d910a9480cd150c656605485
Lukej04 <138320702+Lukej04@users.noreply.github.com>
2024-03-15 19:33:18 -0400

    Update getinfo.js

--- getinfo.js
+++ getinfo.js
@@ -1,8 +1,10 @@
 function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vSapPuH4RuNbKzYEgxx-oqM_jz8klgXS8R_aR7D2b2ZYkzyJVYNthqZvRqpohMToCneB7JrRToGSk0_/pubhtml',
-callback: function(data, tabletop) {
-console.log(data)
-},
-simpleSheet: true } )
-}
+          Papa.parse( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vSapPuH4RuNbKzYEgxx-oqM_jz8klgXS8R_aR7D2b2ZYkzyJVYNthqZvRqpohMToCneB7JrRToGSk0_/pub?output=csv',
+          download: true,
+          header: true,
+          complete: function(results) {
+            var data = results.data
+            console.log(data)
+          }
+        })
 window.addEventListener(‘DOMContentLoaded’, init)
